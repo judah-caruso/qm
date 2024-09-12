541196343b7fd03cba24b95ff2186e186d3c7cc4
Judah Caruso <judahrcr@gmail.com>
2024-09-12 13:40:09 -0600

    update fallback procedures

--- vec2.fallback.go
+++ vec2.fallback.go
@@ -2,18 +2,18 @@
 
 package qm
 
-func add_array_2_float32(l, r [2]float32) [2]float32 {
+func add_arrayof_2float32(l, r [2]float32) [2]float32 {
 	return [2]float32{l[0] + r[0], l[1] + r[1]}
 }
 
-func sub_array_2_float32(l, r [2]float32) [2]float32 {
+func sub_arrayof_2float32(l, r [2]float32) [2]float32 {
 	return [2]float32{l[0] - r[0], l[1] - r[1]}
 }
 
-func mul_array_2_float32(l, r [2]float32) [2]float32 {
+func mul_arrayof_2float32(l, r [2]float32) [2]float32 {
 	return [2]float32{l[0] * r[0], l[1] * r[1]}
 }
 
-func div_array_2_float32(l, r [2]float32) [2]float32 {
+func div_arrayof_2float32(l, r [2]float32) [2]float32 {
 	return [2]float32{l[0] / r[0], l[1] / r[1]}
 }
